b4ddf1a466700de7f2e1bf5353d225d3057136c4
yud0uhu <b2190280@photon.chitose.ac.jp>
2020-12-06 15:28:27 +0900

    コミット

---
+++ Assets/_Assets/GOTCHA/Scripts/SceneManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SilCilSystem.Singletons;
+
+public class SceneManager : MonoBehaviour
+{
+    [SerializeField] private string m_nextSceneName = default;
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            Debug.Log("hoge");
+            SceneLoader.LoadScene(m_nextSceneName);
+        }
+    }
+}
