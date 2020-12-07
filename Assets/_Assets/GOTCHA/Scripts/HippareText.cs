578e0e91f4aaf5b952e600caaf7c4041e666c667
kugimasa <kugimania@gmail.com>
2020-12-07 14:40:54 +0900

    シーン遷移の条件を実装

---
+++ Assets/_Assets/GOTCHA/Scripts/HippareText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using SilCilSystem.Variables;
+using System;
+
+public class HippareText : MonoBehaviour
+{
+    [Header("Variables")]
+    [SerializeField] private VariableFloat m_textScaleY;
+
+    private RectTransform m_rectTransform;
+
+    private void Start()
+    {
+        m_rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_rectTransform.localScale = new Vector3(1, m_textScaleY, 1);
+    }
+}
---
+++ Assets/_Assets/GOTCHA/Scripts/TimeLineManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimeLineManager : MonoBehaviour
+{
+    private PlayableDirector m_playableDirector;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_playableDirector = GetComponent<PlayableDirector>();
+    }
+
+    public void PlayTimeLine()
+    {
+        m_playableDirector.Play();
+    }
+}
