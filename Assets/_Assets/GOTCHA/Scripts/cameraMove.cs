bd7540426da2a33dcd6826ab9e8de58c579d586b
yud0uhu <b2190280@photon.chitose.ac.jp>
2020-12-08 21:46:18 +0900

    カメラパラメータ調整スクリプト

---
+++ Assets/_Assets/GOTCHA/Scripts/cameraMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using SilCilSystem.Variables;
+using System;
+
+public class cameraMove : MonoBehaviour
+{
+    
+    [Header("Variables")]
+    [SerializeField] private VariableFloat m_cameraFOV;
+    [SerializeField] private VariableFloat m_positionY;
+
+    private Transform m_transform;
+    private Camera m_camera;
+
+    void Start()
+    {
+        m_transform = GetComponent<Transform>();
+        m_camera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        m_transform.position = new Vector3(540,m_positionY,-4250);
+        m_camera.fieldOfView = m_cameraFOV;
+    }
+}
