b1d701d4489e62f1bc73c118b7f51c72204d73bd
yud0uhu <b2190280@photon.chitose.ac.jp>
2020-12-08 23:35:59 +0900

    ガチャブタチャンアニメーションスクリプト

---
+++ Assets/_Assets/GOTCHA/Scripts/GotchaPigMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using SilCilSystem.Variables;
+using System;
+
+public class GotchaPigMove : MonoBehaviour
+{
+    [Header("Variables")]
+    [SerializeField] private VariableFloat m_rotation;
+    [SerializeField] private VariableFloat m_position;
+
+    private RectTransform m_rectTransform;
+
+    void Start()
+    {
+        m_rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        m_rectTransform.localPosition = new Vector3(0,m_position,0);
+        m_rectTransform.localRotation = Quaternion.Euler(0,0,m_rotation);
+    }
+}
